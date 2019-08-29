12e2a31e340f224ae5e39dce699d9afc7905895d
Luca Deluigi <luca.deluigi3@studio.unibo.it>
2019-08-30 01:00:36 +0200

    Started custom visualization (not working)

---
+++ examples/Flocking/src/visualisation/CustomVisualisation.h
@@ -0,0 +1,45 @@
+#ifndef __VISUALISATION_H
+#define __VISUALISATION_H
+
+//// cuda keywords
+//#include "cuda_runtime.h"
+//#include "device_launch_parameters.h"
+//
+//// includes, project
+//#include <cuda_runtime.h>
+//#include <stdlib.h>
+//#include <stdio.h>
+//#include <string.h>
+//#include <cmath>
+//
+//// openGL and cuda support
+//#include <GL/glew.h>
+//#include <GL/freeglut.h>
+//#include <cuda_gl_interop.h>
+//
+//// simulation hook
+//#include "header.h"
+//
+//
+//#define SIMULATION_DELAY 1
+//
+//// constants
+//const unsigned int WINDOW_WIDTH = 1280;
+//const unsigned int WINDOW_HEIGHT = 720;
+//
+////frustrum
+//const float NEAR_CLIP = 0.1f;
+//const float FAR_CLIP = 300;
+//
+////Circle model fidelity
+//const int SPHERE_SLICES = 8;
+//const int SPHERE_STACKS = 8;
+//const float SPHERE_RADIUS = 0.1f;
+//
+////Viewing Distance
+//const float VIEW_DISTANCE = 16;
+//
+////light position
+//GLfloat LIGHT_POSITION[] = { 10.0f, 10.0f, 10.0f, 1.0f };
+
+#endif //__VISUALISATION_H
